2591f17d116c03161c8f0c0c6116914699f8803a
Ricardo <ricardoappaiva@gmail.com>
2020-02-21 21:29:33 +0000

    Updates to the way the idea is going to be

--- client/src/Components/Dashboard.js
+++ client/src/Components/Dashboard.js
@@ -7,67 +7,86 @@ import {IdeaContainer} from './IdeaContainer';
 import {IdeaDisplayer} from './IdeaDisplayer';
 import axios from "axios";
 import { Button } from '@material-ui/core';
-
+import IdeaBar from "./IdeaBar";
 export class Dashboard extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-
+          areIdeasDisplayed:false,
+          listOfTitles: [],
+          listOfCategories:[],
+          listOfDescriptions: []
         };
     }
 
     //Gets all ideas and displayes their title
     onClick = async e => {
+
+
+      this.setState({
+        areIdeasDisplayed: true
+      });
       try {
           const res = await axios.get("/api/ideas");
           const ideasTitleList = [];
           const ideasCategoryList = [];
+          const descriptionsList = [];
+
 
           console.log(res.data);
 
           res.data.forEach(idea => ideasTitleList.push(idea.Title) );
           res.data.forEach(idea => ideasCategoryList.push(idea.category) );
-
-          for (let index = 0; index < ideasTitleList.length; index++) {
-            
-            var newElement = document.createElement('div');
-            //styling
-            newElement.style.backgroundColor = 'pink'; 
-            newElement.style.borderRadius = '20px';
-
-            //--------end styling-----------
-            newElement.id = ideasTitleList[index];
-            newElement.className = styles.displayDiv;
-            newElement.innerText= ideasTitleList[index];
-            let component = "<div> <label>  </label </div>"
-            newElement.innerHTML = "<div> <label> Idea Title: " +  ideasTitleList[index] + " </label><br/><br/><label>Idea Category:" +ideasCategoryList[index] + " </label> <br/><br/><br/></div>";
-            document.body.appendChild(newElement);
-          }
+          res.data.forEach(idea => descriptionsList.push(idea.description) );
+          
+          this.setState({listOfTitles:ideasTitleList})
+          this.setState({listOfCategories: ideasCategoryList})
+          this.setState({listOfDescriptions:descriptionsList})
 
       }catch (err){
           console.log(err);
       }
+
     }
 
       
     render() {
-      return (
-        <div >
-            <div className = {styles.dashboardNavigationBar}>
+      if(this.state.areIdeasDisplayed){
+        return(
+          <div >
+              <div className = {styles.dashboardNavigationBar}>
+                {this.props.children}
+                <Button> Home </Button>
+                <Button> Ideas </Button>
+                <Button> Help </Button>
+              </div>
+              <span> </span>
+              <span> </span>
+              {
+                this.state.listOfTitles.map(function(idea, ideax){
+                  return (<IdeaBar title={idea}> </IdeaBar> )
+                })
+              }
               
-              <Button> Home </Button>
-              <Button onClick = {this.onClick}> Ideas </Button>
-              <Button> Help </Button>
+          </div>
+        );
+      } else {
+        return (
+          <div >
+              <div className = {styles.dashboardNavigationBar}>
+                {this.props.children}
+                <Button> Home </Button>
+                <Button onClick = {this.onClick}> Ideas </Button>
+                <Button> Help </Button>
 
-            </div>
-            <span> </span>
-            <span></span>
-            <div>
-              {/* <IdeaContainer/>
-              <IdeaDisplayer/> */}
-            </div>
-        </div>
-      );
+              </div>
+              <span> </span>
+              <span> </span>
+              
+          </div>
+        );
     }
-
   }
+
+
+}
